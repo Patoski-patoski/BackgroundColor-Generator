f024b2c45329c1c98d9caa2455c57cdf581457db
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2022-12-14 18:06:52 +0100

    background generator

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-hello
+// const prompt = require('prompt-sync')()
+
+let copytBtn = document.querySelector('button'); 
+
+
+
+function backgroundColor() {
+
+    let x = Math.floor(Math.random()*255)
+    let y = Math.floor(Math.random()*255)
+    let z = Math.floor(Math.random()*255)
+
+    document.body.style.backgroundColor = `rgb(${x},${y},${z})`    
+    copytBtn.style.backgroundColor = document.body.style.backgroundColor;
+
+    copytBtn.innerHTML = `rgb(${x},${y},${z})`;
+}
+
+let display = setInterval(backgroundColor, 2000)   
+
+
+function myFunc(){
+    let copytBtn = document.querySelector('button').innerHTML; 
+    document.querySelector('span').innerHTML = `copied succesfully: ${copytBtn}`
+
+}
+ 
